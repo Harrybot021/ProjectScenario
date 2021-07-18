af0dcdd405fa1d0b99697f56f104400e7b0ddaa2
Akash Hazra <65800078+IMAkashhazra@users.noreply.github.com>
2021-07-18 12:08:04 +0530

    Update admin.py

--- ScenarioRobot/modules/admin.py
+++ ScenarioRobot/modules/admin.py
@@ -5,9 +5,9 @@ from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, Filters, run_async
 from telegram.utils.helpers import mention_html
 
-from Tianabot import DRAGONS, dispatcher
-from Tianabot.modules.disable import DisableAbleCommandHandler
-from Tianabot.modules.helper_funcs.chat_status import (
+from ScenarioRobot import DRAGONS, dispatcher
+from ScenarioRobot.modules.disable import DisableAbleCommandHandler
+from ScenarioRobot.modules.helper_funcs.chat_status import (
     bot_admin,
     can_pin,
     can_promote,
@@ -15,7 +15,7 @@ from Tianabot.modules.helper_funcs.chat_status import (
     user_admin,
     ADMIN_CACHE,
 )
-from Tianabot.helper_extra.admin_rights import (
+from ScenarioRobot.helper_extra.admin_rights import (
     user_can_pin,
     user_can_promote,
     user_can_changeinfo,
@@ -25,9 +25,9 @@ from Tianabot.modules.helper_funcs.extraction import (
     extract_user,
     extract_user_and_text,
 )
-from Tianabot.modules.log_channel import loggable
-from Tianabot.modules.helper_funcs.alternate import send_message
-from Tianabot.modules.helper_funcs.alternate import typing_action
+from ScenarioRobot.modules.log_channel import loggable
+from ScenarioRobot.modules.helper_funcs.alternate import send_message
+from ScenarioRobot.modules.helper_funcs.alternate import typing_action
 
 
 @run_async
