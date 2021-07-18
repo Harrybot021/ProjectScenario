5fa6d7438eda182fd599d7c58259e2790b2c4f16
Akash Hazra <65800078+IMAkashhazra@users.noreply.github.com>
2021-07-18 12:03:24 +0530

    Update _pyrosongs.py

--- ScenarioRobot/modules/_pyrosongs.py
+++ ScenarioRobot/modules/_pyrosongs.py
@@ -7,9 +7,9 @@ import aiohttp
 import youtube_dl
 
 from pyrogram import filters
-from Tianabot import pbot
+from ScenarioRobot import pbot
 from youtube_search import YoutubeSearch
-from Tianabot.pyrogramee.errors import capture_err
+from ScenarioRobot.pyrogramee.errors import capture_err
 
 
 def time_to_seconds(time):
@@ -57,7 +57,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f'🎙 **Title**: [{title[:35]}]({link})\n🎬 **Source**: YouTube\n⏱️ **Duration**: `{duration}`\n👁‍🗨 **Views**: `{views}`\n📤 **By**: @Rikudo_senin_bot'
+        rep = f'🎙 **Title**: [{title[:35]}]({link})\n🎬 **Source**: YouTube\n⏱️ **Duration**: `{duration}`\n👁‍🗨 **Views**: `{views}`\n📤 **By**: @Offline_Walker'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
