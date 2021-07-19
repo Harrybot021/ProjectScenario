ba8d70c6bf536a2291f7c2d52f5e6a9c07716f6d
Akash Hazra <65800078+IMAkashhazra@users.noreply.github.com>
2021-07-19 19:31:06 +0530

    Update __init__.py

--- ScenarioRobot/sql/__init__.py
+++ ScenarioRobot/sql/__init__.py
@@ -1,4 +1,4 @@
-from Tianabot import DB_URI
+from ScenarioRobot import DB_URI
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
