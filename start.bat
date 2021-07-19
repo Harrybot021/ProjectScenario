fc10e68093346bc0a2aaa46da3aed719b842f3ef
Akash Hazra <65800078+IMAkashhazra@users.noreply.github.com>
2021-07-19 18:50:06 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
-TITLE TIANA BOT
+TITLE SCENARIO ROBOT
 :: Enables virtual env mode and then starts Tiana
-env\scripts\activate.bat && py -m Tianabot
+env\scripts\activate.bat && py -m ScenarioRobot
