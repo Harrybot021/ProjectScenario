e21071d60c6b647762cb292ae13b2f79d963802e
Akash Hazra <65800078+IMAkashhazra@users.noreply.github.com>
2021-07-19 21:43:36 +0530

    Update connection_sql.py

--- ScenarioRobot/modules/sql/connection_sql.py
+++ ScenarioRobot/modules/sql/connection_sql.py
@@ -4,7 +4,7 @@ from typing import Union
 
 from sqlalchemy import Column, String, Boolean, UnicodeText, Integer
 
-from Tianabot.modules.sql import SESSION, BASE
+from ScenarioRobot.modules.sql import SESSION, BASE
 
 
 class ChatAccessConnectionSettings(BASE):
