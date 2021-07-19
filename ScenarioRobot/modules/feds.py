ec7233cdd96b0bef67589afeac95dbda01592cdb
Akash Hazra <65800078+IMAkashhazra@users.noreply.github.com>
2021-07-19 23:52:34 +0530

    Update feds.py

--- ScenarioRobot/modules/feds.py
+++ ScenarioRobot/modules/feds.py
@@ -6,8 +6,8 @@ import time
 import uuid
 from io import BytesIO
 
-import Tianabot.modules.sql.feds_sql as sql
-from Tianabot import (
+import ScenarioRobot.modules.sql.feds_sql as sql
+from ScenarioRobot import (
     EVENT_LOGS,
     LOGGER,
     SUPPORT_CHAT,
@@ -17,15 +17,15 @@ from Tianabot import (
     WOLVES,
     dispatcher,
 )
-from Tianabot.modules.disable import DisableAbleCommandHandler
-from Tianabot.modules.helper_funcs.alternate import send_message
-from Tianabot.modules.helper_funcs.chat_status import is_user_admin
-from Tianabot.modules.helper_funcs.extraction import (
+from ScenarioRobot.modules.disable import DisableAbleCommandHandler
+from ScenarioRobot.modules.helper_funcs.alternate import send_message
+from ScenarioRobot.modules.helper_funcs.chat_status import is_user_admin
+from ScenarioRobot.modules.helper_funcs.extraction import (
     extract_unt_fedban,
     extract_user,
     extract_user_fban,
 )
-from Tianabot.modules.helper_funcs.string_handling import markdown_parser
+from ScenarioRobot.modules.helper_funcs.string_handling import markdown_parser
 from telegram import (
     InlineKeyboardButton,
     InlineKeyboardMarkup,
