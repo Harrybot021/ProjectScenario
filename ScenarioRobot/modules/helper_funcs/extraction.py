cc90a85878d064ebfd2a1bd3816a3e4998874178
Akash Hazra <65800078+IMAkashhazra@users.noreply.github.com>
2021-07-19 21:07:09 +0530

    Update extraction.py

--- ScenarioRobot/modules/helper_funcs/extraction.py
+++ ScenarioRobot/modules/helper_funcs/extraction.py
@@ -1,7 +1,7 @@
 from typing import List, Optional
 
-from Tianabot import LOGGER
-from Tianabot.modules.users import get_user_id
+from ScenarioRobot import LOGGER
+from ScenarioRobot.modules.users import get_user_id
 from telegram import Message, MessageEntity
 from telegram.error import BadRequest
 
