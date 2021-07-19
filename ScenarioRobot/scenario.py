eef1d3423f7bec44594083c0e9a86e3a9b44db06
Akash Hazra <65800078+IMAkashhazra@users.noreply.github.com>
2021-07-19 20:04:45 +0530

    Update scenario.py

--- ScenarioRobot/scenario.py
+++ ScenarioRobot/scenario.py
@@ -1,2 +1,14 @@
 from ScenarioRobot import telethn
 from telethon import events
+
+
+def scenario(**args):
+    """New message."""
+    patern = args.get("pattern', None)
+    r_pattern = r'^[/l]'
+    if pattern is not None and not pattern.startwith('(?i)'):
+        args['pattern'] = '(?i)' + pattern
+    args['pattern'] = pattern.replace('^/', r_pattern, 1)
+
+
+
