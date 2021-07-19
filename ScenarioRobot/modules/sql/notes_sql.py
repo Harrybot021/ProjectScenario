4ebd5d86934d7df20f779d3fcc79f8ca0135f01f
Akash Hazra <65800078+IMAkashhazra@users.noreply.github.com>
2021-07-19 22:16:50 +0530

    Update notes_sql.py

--- ScenarioRobot/modules/sql/notes_sql.py
+++ ScenarioRobot/modules/sql/notes_sql.py
@@ -1,8 +1,8 @@
 # Note: chat_id's are stored as strings because the int is too large to be stored in a PSQL database.
 import threading
 
-from Tianabot.modules.helper_funcs.msg_types import Types
-from Tianabot.modules.sql import BASE, SESSION
+from ScenarioRobot.modules.helper_funcs.msg_types import Types
+from ScenarioRobot.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, Integer, String, UnicodeText, distinct, func
 
 
