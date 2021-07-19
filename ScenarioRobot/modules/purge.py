726684c10bb0467d4a8e2cd874efe456c7da7cf2
Akash Hazra <65800078+IMAkashhazra@users.noreply.github.com>
2021-07-20 00:33:14 +0530

    Update purge.py

--- ScenarioRobot/modules/purge.py
+++ ScenarioRobot/modules/purge.py
@@ -1,8 +1,8 @@
 import time
 from telethon import events
 
-from Tianabot import telethn
-from Tianabot.modules.helper_funcs.telethn.chatstatus import (
+from ScenarioRobot import telethn
+from ScenarioRobot.modules.helper_funcs.telethn.chatstatus import (
     can_delete_messages,
     user_is_admin,
 )
