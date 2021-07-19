12b331084a0f3ee84b233bf01747582ad5cc2915
Akash Hazra <65800078+IMAkashhazra@users.noreply.github.com>
2021-07-19 21:05:25 +0530

    Update chat_status.py

--- ScenarioRobot/modules/helper_funcs/chat_status.py
+++ ScenarioRobot/modules/helper_funcs/chat_status.py
@@ -403,6 +403,6 @@ def connection_status(func):
 
 
 # Workaround for circular import with connection.py
-from Tianabot.modules import connection
+from ScenarioRobot.modules import connection
 
 connected = connection.connected
