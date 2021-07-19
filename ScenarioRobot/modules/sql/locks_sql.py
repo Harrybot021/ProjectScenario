3277777d741ddc68bcce744b662fdf40264eced4
Akash Hazra <65800078+IMAkashhazra@users.noreply.github.com>
2021-07-19 21:55:53 +0530

    Update locks_sql.py

--- ScenarioRobot/modules/sql/locks_sql.py
+++ ScenarioRobot/modules/sql/locks_sql.py
@@ -3,7 +3,7 @@ import threading
 
 from sqlalchemy import Column, String, Boolean
 
-from Tianabot.modules.sql import SESSION, BASE
+from ScenarioRobot.modules.sql import SESSION, BASE
 
 
 class Permissions(BASE):
