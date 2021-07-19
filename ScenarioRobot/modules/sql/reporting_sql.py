e047bf54bfc4ec9d7ca4a3a22f651766c3f2ca22
Akash Hazra <65800078+IMAkashhazra@users.noreply.github.com>
2021-07-19 22:18:20 +0530

    Update reporting_sql.py

--- ScenarioRobot/modules/sql/reporting_sql.py
+++ ScenarioRobot/modules/sql/reporting_sql.py
@@ -1,7 +1,7 @@
 import threading
 from typing import Union
 
-from Tianabot.modules.sql import BASE, SESSION
+from ScenarioRobot.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, Integer, String
 
 
