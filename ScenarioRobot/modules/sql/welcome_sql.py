fd4c42567788ee66a779efe81b79103ed2b94f65
Akash Hazra <65800078+IMAkashhazra@users.noreply.github.com>
2021-07-19 22:27:59 +0530

    Update welcome_sql.py

--- ScenarioRobot/modules/sql/welcome_sql.py
+++ ScenarioRobot/modules/sql/welcome_sql.py
@@ -2,8 +2,8 @@ import random
 import threading
 from typing import Union
 
-from Tianabot.modules.helper_funcs.msg_types import Types
-from Tianabot.modules.sql import BASE, SESSION
+from ScenarioRobot.modules.helper_funcs.msg_types import Types
+from ScenarioRobot.modules.sql import BASE, SESSION
 from sqlalchemy import BigInteger, Boolean, Column, Integer, String, UnicodeText
 
 DEFAULT_WELCOME = "Hey {first}, how are you?"
