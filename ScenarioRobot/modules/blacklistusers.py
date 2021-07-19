b43462fe05a581e2b1f03cd689c551b4e87d51a2
Akash Hazra <65800078+IMAkashhazra@users.noreply.github.com>
2021-07-19 20:54:38 +0530

    Update blacklistusers.py

--- ScenarioRobot/modules/blacklistusers.py
+++ ScenarioRobot/modules/blacklistusers.py
@@ -1,7 +1,7 @@
 # Module to blacklist users and prevent them from using commands by @TheRealPhoenix
 import html
-import Tianabot.modules.sql.blacklistusers_sql as sql
-from Tianabot import (
+import ScenarioRobot.modules.sql.blacklistusers_sql as sql
+from ScenarioRobot import (
     DEV_USERS,
     OWNER_ID,
     DRAGONS,
@@ -10,12 +10,12 @@ from Tianabot import (
     WOLVES,
     dispatcher,
 )
-from Tianabot.modules.helper_funcs.chat_status import dev_plus
-from Tianabot.modules.helper_funcs.extraction import (
+from ScenarioRobot.modules.helper_funcs.chat_status import dev_plus
+from ScenarioRobot.modules.helper_funcs.extraction import (
     extract_user,
     extract_user_and_text,
 )
-from Tianabot.modules.log_channel import gloggable
+from ScenarioRobot.modules.log_channel import gloggable
 from telegram import ParseMode, Update
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, run_async
