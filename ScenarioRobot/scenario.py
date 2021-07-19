54dc1e8f007143ba13aecd67fc65102482e32391
Akash Hazra <65800078+IMAkashhazra@users.noreply.github.com>
2021-07-19 20:16:19 +0530

    Update scenario.py

--- ScenarioRobot/scenario.py
+++ ScenarioRobot/scenario.py
@@ -10,5 +10,21 @@ def scenario(**args):
         args['pattern'] = '(?i)' + pattern
     args['pattern'] = pattern.replace('^/', r_pattern, 1)
 
-
-
+    def decorator(func):
+        telethon.add_event_handler(func, events.NewMessage(**args))
+        return func
+                      
+    return decorator
+                      
+     
+def inlinequery(**args):
+    """Inline query."""                 
+    pattern = args.get('pattern', None)              
+    if pattern is not None and not pattern.startswith('(?i)'):                  
+        args['pattern'] = '(?i)' + pattern 
+                      
+    def decorator(func):              
+        telethn.add_event_handler(func, event.InlineQuery(**args))            
+        return func              
+  
+    return decorator                  
