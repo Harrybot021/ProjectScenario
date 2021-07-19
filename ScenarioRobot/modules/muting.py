776170e913b318204e71e25b0567005eaab9be57
Akash Hazra <65800078+IMAkashhazra@users.noreply.github.com>
2021-07-20 00:24:15 +0530

    Update muting.py

--- ScenarioRobot/modules/muting.py
+++ ScenarioRobot/modules/muting.py
@@ -1,20 +1,20 @@
 import html
 from typing import Optional
 
-from Tianabot import LOGGER, TIGERS, dispatcher
-from Tianabot.modules.helper_funcs.chat_status import (
+from ScenarioRobot import LOGGER, TIGERS, dispatcher
+from ScenarioRobot.modules.helper_funcs.chat_status import (
     bot_admin,
     can_restrict,
     connection_status,
     is_user_admin,
     user_admin,
 )
-from Tianabot.modules.helper_funcs.extraction import (
+from ScenarioRobot.modules.helper_funcs.extraction import (
     extract_user,
     extract_user_and_text,
 )
-from Tianabot.modules.helper_funcs.string_handling import extract_time
-from Tianabot.modules.log_channel import loggable
+from ScenarioRobot.modules.helper_funcs.string_handling import extract_time
+from ScenarioRobot.modules.log_channel import loggable
 from telegram import Bot, Chat, ChatPermissions, ParseMode, Update
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, run_async
