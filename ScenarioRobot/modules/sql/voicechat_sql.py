ea01dd94e004981b7d0b583c9a32c32adc043170
Akash Hazra <65800078+IMAkashhazra@users.noreply.github.com>
2021-07-19 22:24:46 +0530

    Update voicechat_sql.py

--- ScenarioRobot/modules/sql/voicechat_sql.py
+++ ScenarioRobot/modules/sql/voicechat_sql.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Boolean, Column, Integer, String, UnicodeText
-from Tianabot.modules.sql import BASE, SESSION
+from ScenarioRobot.modules.sql import BASE, SESSION
 
 
 class Talkmode(BASE):
