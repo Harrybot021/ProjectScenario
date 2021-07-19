8263d9d32a0d6a8005087e01b860e96104d26819
Akash Hazra <65800078+IMAkashhazra@users.noreply.github.com>
2021-07-19 21:14:39 +0530

    Update misc.py

--- ScenarioRobot/modules/helper_funcs/misc.py
+++ ScenarioRobot/modules/helper_funcs/misc.py
@@ -4,7 +4,7 @@ from typing import List, Dict
 from telegram import MAX_MESSAGE_LENGTH, InlineKeyboardButton, Bot, ParseMode,Update
 from telegram.error import TelegramError
 
-from Tianabot import LOAD, NO_LOAD
+from ScenarioRobot import LOAD, NO_LOAD
 
 
 class EqInlineKeyboardButton(InlineKeyboardButton):
